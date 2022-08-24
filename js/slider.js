a2dc2eeba3bdbd48342a85966434615eb77f26fb
M6Irinel <momoloacairinel666@gmail.com>
2022-08-24 14:30:51 +0200

    first commit

---
+++ js/slider.js
@@ -0,0 +1,31 @@
+const slides = [
+	'./img/01.jpg',
+	'./img/02.jpg',
+	'./img/03.jpg',
+	'./img/04.jpg',
+	'./img/05.jpg',
+];
+
+new Vue( {
+	el: '#slider',
+	data () {
+		return {
+			slides,
+			i: 0
+		}
+	},
+	methods: {
+		present ( id ) {
+			if ( this.i === id )
+				return 'active';
+		},
+		next () {
+			if ( ++this.i >= slides.length )
+				this.i = 0;
+		},
+		prev () {
+			if ( --this.i < 0 )
+				this.i = slides.length - 1;
+		}
+	},
+} );
